838645340aebc975a0be2dcd7096472282477e4b
Aidan Rutherford <62723997+aidanTheCoder@users.noreply.github.com>
2021-12-09 18:38:17 -0700

    Delete Prescott Animal Shelter HTML JS CSS directory

--- Prescott Animal Shelter HTML JS CSS/Prescott Animal Shelter HTML JS CSS/connector.php
+++
@@ -1,3 +0,0 @@
-<?php
-	echo "connector.php running";
-?>
--- Prescott Animal Shelter HTML JS CSS/animal_search.js
+++
@@ -1,5 +0,0 @@
-function selectAll(source, type) {
-	selectAllThatApplyBoxes = document.getElementsByName(type);
-	for(var i = 0, n = selectAllThatApplyBoxes.length; i < n; ++i)
-		selectAllThatApplyBoxes[i].checked = source.checked;
-}
